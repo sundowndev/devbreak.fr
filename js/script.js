b8149035655fb9aeb1547ff596f2c24882210f61
sundowndev <raphael@crvx.fr>
2018-12-18 18:42:35 +0100

    Index & script

--- js/script.js
+++ js/script.js
@@ -1,33 +1,71 @@
-/*var collapseButtons = document.querySelectorAll('.panel-header-right a');
-
-collapseButtons.forEach(function(btn){
-    btn.addEventListener('click', function(e){
-        e.preventDefault();
-        
-        var panel = btn.parentElement.parentElement.parentElement;
-        var icon = btn.childNodes[0];
-        
-        if(panel.className == 'panel collapse'){
-            panel.classList.remove('collapse');
-        }else{
-            panel.classList.add('collapse');
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
         }
+    }
+    rawFile.send(null);
+}
+
+readTextFile("/js/data.json", function(text){
+    const obj = JSON.parse(text);
+    console.log(obj);
+
+    const sections = document.getElementById('tools');
+
+    obj.data.forEach(function(section) {
+      let categoriesHtml = ``;
+
+      section.categories.forEach(function(category) {
+        let toolsHtml = ``;
+
+        category.links.forEach(function(link) {
+          toolsHtml += `<li>
+              <a href="${link.url}" target="_blank">${link.title}</a>
+              <span>${link.text}</span>
+          </li>`;
+        });
+
+        categoriesHtml += `<div class="panel-content-category">
+            <h3>${category.title}</h3>
+
+            <ul>${toolsHtml}</ul>
+        </div>`;
+      });
+
+      let sectionHtml = `<div class="panel collapse">
+          <div class="panel-header">
+              <p><i class="ion-${section.icon}"></i> ${section.name}</p>
+              <div class="panel-header-right">
+                  <i class="arrow ion-arrow-down-b"></i>
+              </div>
+          </div>
+
+          <div class="panel-content">
+              ${categoriesHtml}
+          </div>
+      </div>`;
+
+      sections.innerHTML += sectionHtml;
     });
-});*/
 
-var panels = document.querySelectorAll('.panel-header');
+    const panels = document.querySelectorAll('.panel-header');
 
-panels.forEach(function(panel){
-    panel.addEventListener('click', function(e){
-        e.preventDefault();
-        
-        var panelParent = panel.parentElement;
-        var icon = panel.childNodes[0].childNodes[0];
-        
-        if(panelParent.className == 'panel collapse'){
-            panelParent.classList.remove('collapse');
-        }else{
-            panelParent.classList.add('collapse');
-        }
+    panels.forEach(function(panel){
+        panel.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const panelParent = panel.parentElement;
+            const icon = panel.childNodes[0].childNodes[0];
+
+            if (panelParent.className == 'panel collapse') {
+                panelParent.classList.remove('collapse');
+            } else {
+                panelParent.classList.add('collapse');
+            }
+        });
     });
 });
