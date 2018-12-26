02839a15d4041a45b60c5206d6648320228449f1
sundowndev <raphael@crvx.fr>
2018-12-26 17:14:46 +0100

    Use js to parse data instead of json

---
+++ js/data.js
@@ -0,0 +1,293 @@
+const json = {
+  data: [
+    {
+      name: 'Web design',
+      icon: 'paintbrush',
+      categories: [
+        {
+          title: 'Tools',
+          links: [
+            {
+              title: 'coolors.co',
+              text: 'The super fast color schemes generator',
+              url: 'https://coolors.co/'
+            },
+            {
+              title: 'flatuicolors.com',
+              text: 'Copy Paste Color Pallette from Flat UI Theme',
+              url: 'https://flatuicolors.com/'
+            },
+            {
+              title: 'materialui.co',
+              text: 'Material Color Palette, Flat UI Color Palette, Icons & more.',
+              url: 'https://materialui.co/'
+            },
+            {
+              title: 'color.adobe.com',
+              text: 'Create color schemes with the color wheel or browse thousands of color combinations from the Color community.',
+              url: 'https://color.adobe.com/'
+            }
+          ]
+        },
+        {
+          title: 'Mockup',
+          links: [
+            {
+              title: 'threed.io',
+              text: 'Generate 3D mockups right in your browser.',
+              url: 'https://threed.io/'
+            },
+            {
+              title: 'pixeden.com',
+              text: 'Free web design templates.',
+              url: 'https://pixeden.com/'
+            },
+            {
+              title: 'graphberry.com',
+              text: 'Free pixel perfect resources, made with love.',
+              url: 'https://graphberry.com/'
+            },
+            {
+              title: 'mockflow.com',
+              text: 'Your collaborative whiteboard for brainstorming UI.',
+              url: 'https://mockflow.com/'
+            },
+            {
+              title: 'wireframe.cc',
+              text: 'A better wireframing experience.',
+              url: 'https://wireframe.cc/'
+            },
+            {
+              title: 'framebox.org',
+              text: 'Easy frame sharing.',
+              url: 'https://framebox.org/'
+            }
+          ]
+        },
+        {
+          title: 'Extensions',
+          links: [
+            {
+              title: 'csspeeper.com',
+              text: 'Inspect styles in a simple, well-organized & beautiful way with our Chrome extension.',
+              url: 'https://csspeeper.com/'
+            },
+            {
+              title: 'fontface.ninja',
+              text: 'Inspect, try, buy and bookmark any fonts on any website.',
+              url: 'https://fontface.ninja/'
+            },
+            {
+              title: 'wappalyzer.com',
+              text: 'Identify technology on websites.',
+              url: 'https://wappalyzer.com/'
+            },
+            {
+              title: 'producthunt.com',
+              text: 'Discover your next favorite thing.',
+              url: 'https://producthunt.com/'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: 'Resources',
+      icon: 'folder',
+      categories: [
+        {
+          title: 'Videos',
+          links: [
+            {
+              title: 'videos.pexels.com',
+              text: 'Completely free stock videos.',
+              url: 'https://videos.pexels.com/'
+            },
+            {
+              title: 'coverr.co',
+              text: 'Beautiful, free videos for your homepage.',
+              url: 'https://coverr.co/'
+            },
+            {
+              title: 'videvo.net',
+              text: 'Free stock HD video footage.',
+              url: 'https://videvo.net/'
+            },
+            {
+              title: 'allthefreestock.com',
+              text: 'One place to find all the free stock images, videos, sound and icons.',
+              url: 'https://allthefreestock.com/'
+            },
+            {
+              title: 'stampede.it',
+              text: 'Audio and video stock library. Free for personal and commercial purposes.',
+              url: 'https://stampede.it/'
+            },
+            {
+              title: 'stockio.com',
+              text: 'Free Photos, Vectors, Icons, Fonts and Videos.',
+              url: 'https://stockio.com/'
+            }
+          ]
+        },
+        {
+          title: 'Images',
+          links: [
+            {
+              title: 'freephotos.cc',
+              text: 'Free beautiful stock photos with Creative Commons (CC) licensing.',
+              url: 'https://freephotos.cc/'
+            },
+            {
+              title: 'everypixel.com',
+              text: 'Stock image search engine powered by AI.',
+              url: 'https://everypixel.com/'
+            },
+            {
+              title: 'visualhunt.com',
+              text: 'We hunt free high quality stock photos.',
+              url: 'https://visualhunt.com/'
+            },
+            {
+              title: 'allthefreestock.com',
+              text: 'One place to find all the free stock images, videos, sound and icons.',
+              url: 'https://allthefreestock.com/'
+            },
+            {
+              title: 'unsplash.com',
+              text: 'Beautiful, free photos gifted by the world’s most generous community of photographers.',
+              url: 'https://unsplash.com/'
+            },
+            {
+              title: 'thestocks.im',
+              text: 'Hand-picked free photos for your inspiration.',
+              url: 'https://thestocks.im/'
+            },
+            {
+              title: 'stockio.com',
+              text: 'Free Photos, Vectors, Icons, Fonts and Videos.',
+              url: 'https://stockio.com/'
+            },
+            {
+              title: 'stampede.it',
+              text: 'Audio and video stock library. Free for personal and commercial purposes.',
+              url: 'https://stampede.it/'
+            },
+            {
+              title: 'stocksnap.io',
+              text: 'Hundreds of high resolution images added weekly.',
+              url: 'https://stocksnap.io/'
+            },
+            {
+              title: 'gettyimages.fr',
+              text: 'Find high resolution royalty-free images, editorial stock photos, vector art and more.',
+              url: 'https://gettyimages.fr/'
+            }
+          ]
+        },
+        {
+          title: 'Music & sounds',
+          links: [
+            {
+              title: 'stampede.it',
+              text: 'Audio and video stock library. Free for personal and commercial purposes.',
+              url: 'https://stampede.it/'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: 'Icons',
+      icon: 'heart',
+      categories: [
+        {
+          title: 'Templates & vector icons',
+          links: [
+            {
+              title: 'flaticon.com',
+              text: 'The largest database of free icons available in PNG, SVG, EPS, PSD and BASE 64 formats.',
+              url: 'https://flaticon.com/'
+            },
+            {
+              title: 'iconfinder.com',
+              text: 'The market place for vector icons in SVG, PNG, CSH and AI format.',
+              url: 'https://iconfinder.com/'
+            },
+            {
+              title: 'material.io',
+              text: 'Access over 900 material system icons, available in a variety of sizes and densities, and as a web font.',
+              url: 'https://material.io/'
+            },
+            {
+              title: 'thenounproject.com',
+              text: 'Over a million curated icons, created by a global community.',
+              url: 'https://thenounproject.com/'
+            }
+          ]
+        },
+        {
+          title: 'Icon fonts',
+          links: [
+            {
+              title: 'ionicons.com',
+              text: 'MIT Licensed css icons font for Ionic Framework. 100% free and open source.',
+              url: 'https://ionicons.com/'
+            },
+            {
+              title: 'fontawesome.io',
+              text: 'The iconic font and CSS toolkit.',
+              url: 'https://fontawesome.io/'
+            },
+            {
+              title: 'icomoon.io',
+              text: 'Pixel perfect icon solutions. Free custom css icon font generator.',
+              url: 'https://icomoon.io/'
+            },
+            {
+              title: 'fontello.com',
+              text: 'Tool to build custom fonts with icons.',
+              url: 'https://fontello.com/'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: 'Fonts',
+      icon: 'edit',
+      categories: [
+        {
+          title: 'Font libraries',
+          links: [
+            {
+              title: 'fontsinuse.com',
+              text: 'An independent archive of typography.',
+              url: 'https://fontsinuse.com/'
+            }
+          ]
+        },
+        {
+          title: 'Web fonts',
+          links: [
+            {
+              title: 'fonts.google.com',
+              text: 'Making the web more beautiful, fast, and open through great typography.',
+              url: 'https://fonts.google.com/'
+            },
+            {
+              title: 'omnibus-type.com',
+              text: 'Provides webfonts at the highest aesthetic and technical level.',
+              url: 'https://omnibus-type.com/'
+            },
+            {
+              title: 'fontsquirrel.com',
+              text: 'Handpicked free fonts for graphic designers with commercial-use licenses.',
+              url: 'https://icomoon.io/'
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
--- js/script.js
+++ js/script.js
@@ -1,71 +1,54 @@
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
+const sections = document.getElementById('tools');
 
-readTextFile("/devbreak.fr/js/data.json", function(text){
-    const obj = JSON.parse(text);
-    console.log(obj);
+json.data.forEach(function(section) {
+  let categoriesHtml = ``;
 
-    const sections = document.getElementById('tools');
+  section.categories.forEach(function(category) {
+    let toolsHtml = ``;
 
-    obj.data.forEach(function(section) {
-      let categoriesHtml = ``;
-
-      section.categories.forEach(function(category) {
-        let toolsHtml = ``;
-
-        category.links.forEach(function(link) {
-          toolsHtml += `<li>
-              <a href="${link.url}" target="_blank">${link.title}</a>
-              <span>${link.text}</span>
-          </li>`;
-        });
-
-        categoriesHtml += `<div class="panel-content-category">
-            <h3>${category.title}</h3>
-
-            <ul>${toolsHtml}</ul>
-        </div>`;
-      });
-
-      let sectionHtml = `<div class="panel collapse">
-          <div class="panel-header">
-              <p><i class="ion-${section.icon}"></i> ${section.name}</p>
-              <div class="panel-header-right">
-                  <i class="arrow ion-arrow-down-b"></i>
-              </div>
-          </div>
-
-          <div class="panel-content">
-              ${categoriesHtml}
-          </div>
-      </div>`;
-
-      sections.innerHTML += sectionHtml;
+    category.links.forEach(function(link) {
+      toolsHtml += `<li>
+          <a href="${link.url}" target="_blank">${link.title}</a>
+          <span>${link.text}</span>
+      </li>`;
     });
 
-    const panels = document.querySelectorAll('.panel-header');
+    categoriesHtml += `<div class="panel-content-category">
+        <h3>${category.title}</h3>
 
-    panels.forEach(function(panel){
-        panel.addEventListener('click', function(e) {
-            e.preventDefault();
+        <ul>${toolsHtml}</ul>
+    </div>`;
+  });
 
-            const panelParent = panel.parentElement;
-            const icon = panel.childNodes[0].childNodes[0];
+  let sectionHtml = `<div class="panel collapse">
+      <div class="panel-header">
+          <p><i class="ion-${section.icon}"></i> ${section.name}</p>
+          <div class="panel-header-right">
+              <i class="arrow ion-arrow-down-b"></i>
+          </div>
+      </div>
 
-            if (panelParent.className == 'panel collapse') {
-                panelParent.classList.remove('collapse');
-            } else {
-                panelParent.classList.add('collapse');
-            }
-        });
+      <div class="panel-content">
+          ${categoriesHtml}
+      </div>
+  </div>`;
+
+  sections.innerHTML += sectionHtml;
+});
+
+const panels = document.querySelectorAll('.panel-header');
+
+panels.forEach(function(panel){
+    panel.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const panelParent = panel.parentElement;
+        const icon = panel.childNodes[0].childNodes[0];
+
+        if (panelParent.className == 'panel collapse') {
+            panelParent.classList.remove('collapse');
+        } else {
+            panelParent.classList.add('collapse');
+        }
     });
 });
